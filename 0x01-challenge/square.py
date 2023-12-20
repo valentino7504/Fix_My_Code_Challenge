23c3326180d773845e683e2322a20d3915845424
Edwin Ade <edwinmbonyjr@gmail.com>
2023-12-20 16:43:16 +0100

    Square class

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,37 +1,38 @@
 #!/usr/bin/python3
+"""
 
+This is a module used to define a square
+
+
+"""
 class Square():
-
+    """
+    A square class
+    """
     width = 0
+    height = 0
 
     def __init__(self, *args, **kwargs):
+        """
+        Init method for square class
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    @property
-    def width(self):
-        """
-        The width property
-        """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("Value is not an integer")
-        """
-        width setter
-        """
-        self.__width = value
-
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PerimeterOfMySquare(self):
-        return (self.width * 4)
+    def permiter_of_my_square(self):
+        """
+        returns the perimeter of the square
+        """
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """
+        this is how the square will be printed
+        """
         return "{}/{}".format(self.width, self.width)
 
 
@@ -40,4 +41,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PerimeterOfMySquare())
+    print(s.permiter_of_my_square())
